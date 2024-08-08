42cefe4fc855bc1d2dddc65d198588fde52f3355
ThiagoRoq <thiago.roque@sasi.com.br>
2024-08-08 10:52:43 -0400

    Update db.py

--- api_publica/api/src/storage/db.py
+++ api_publica/api/src/storage/db.py
@@ -1380,7 +1380,8 @@ def get_produtividade(filters: dict) -> List[Produtividade]:
     if filters.get('range_date'):
         condition += " and DATE(CONVERT_TZ(created_at, '+00:00', '-04:00')) >= %s "
         condition += " and DATE(CONVERT_TZ(created_at, '+00:00', '-04:00')) <= %s "
-        params = filters['range_date'].split(',')
+        for date in filters['range_date'].split(','):
+            params.append(date)
     if filters.get('especific_date'):
         condition += " and DATE(CONVERT_TZ(created_at, '+00:00', '-04:00')) = %s "
         params = [filters['especific_date']]
